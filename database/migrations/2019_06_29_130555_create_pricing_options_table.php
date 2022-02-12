d3edb0e3b198a6c8452ef4720b369beed2b6437b
Rob Burley <rob.burley@boohoo.com>
2022-02-12 22:05:27 +0000

    work in pence instead of floats

--- database/migrations/2019_06_29_130555_create_pricing_options_table.php
+++ database/migrations/2019_06_29_130555_create_pricing_options_table.php
@@ -17,7 +17,7 @@ class CreatePricingOptionsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('type');
-            $table->float('price');
+            $table->integer('price');
             $table->timestamps();
         });
     }
