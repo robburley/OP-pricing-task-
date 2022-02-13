278c23c98d5ca7465f188a1770b572f1c6715645
Rob Burley <rob.burley@boohoo.com>
2022-02-13 00:41:05 +0000

    Add method to interface

--- app/Services/PriceEngine/Modifiers/Modifier.php
+++ app/Services/PriceEngine/Modifiers/Modifier.php
@@ -9,5 +9,7 @@ interface Modifier
 {
     public function __construct(Member $member, Venue $venue);
 
+    public function handle(int $price, array $settings): int;
+
     public function modify(int $price, array $settings): int;
 }
