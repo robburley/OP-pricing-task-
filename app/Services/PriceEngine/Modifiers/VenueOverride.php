f9e6e31d828076b5063f641e26a0f1904a0bbfbf
Rob Burley <rob.burley@boohoo.com>
2022-02-13 00:38:46 +0000

    Add missing venue override logic

--- app/Services/PriceEngine/Modifiers/VenueOverride.php
+++ app/Services/PriceEngine/Modifiers/VenueOverride.php
@@ -6,6 +6,10 @@ class VenueOverride extends BaseModifier
 {
     public function modify(int $price, array $settings): int
     {
+        if (collect($settings['venue_locations'] ?? [])->contains($this->venue->location)) {
+            return $settings['price'] ?? $price;
+        }
+
         return $price;
     }
 }
